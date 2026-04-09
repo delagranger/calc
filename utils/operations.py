e42613235f618d0528c0655f74b31091b26c2dda
Auguste <longwonderstaff@gmail.com>
2026-04-09 17:42:25 +0300

    add math operations block

---
+++ utils/operations.py
@@ -0,0 +1,11 @@
+def Addition(a, b):
+    return a + b
+def Subtraction(a, b):
+    return a - b
+def Multiplication(a, b):
+    return a * b
+def Division(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("Ошибка: на ноль делить нельзя")
