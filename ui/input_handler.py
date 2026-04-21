6644cd4c8f2f64db7689af0ab070c9df845ad538
Auguste <longwonderstaff@gmail.com>
2026-04-21 17:58:28 +0300

    feature/get_answer

--- ui/input_handler.py
+++ ui/input_handler.py
@@ -1,11 +1,12 @@
-def GetData():
-    print("Начало работы")
+def GetOpAndNums():
+    print("Возможноные операции: +, -, *, /")
 
     a = float(input("Введите первое число: "))
-
-    print("Список возможных операций: +, -, *, /, **, //, %")
     op = input("Введите операцию: ")
-
     b = float(input("Введите второе число: "))
     
     return a, op, b
+
+def GetAnswer():
+    answer = input("Впишите действие (Посчитать, История, Выход): ")
+    return answer
