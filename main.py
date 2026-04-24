a28c56d744f6049b3a7c271c6daa50b99a80498b
Auguste <longwonderstaff@gmail.com>
2026-04-24 15:08:22 +0300

    rebuild_structure/main.py

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-from ui.input_handler import GetOpAndNums, GetAnswer
-from ui.greeting import greeting
-from ui.farewell import farewell
+from history.history import show_history, update_history
 
-from operations.math_core import Addition, Subtraction, Multiplication, Division
+from operations.math_core import addition, subtraction, multiplication, division
 
-from history.history import ShowHistory
+from ui.input_handler import get_operation_and_operands, get_command
+from ui.output_handler import print_last_result, print_result, show_commands, greeting, farewell
+
+from utils.validation import *
 
 greeting()
+show_commands()
+
 result = 0
 last_result = 0
 
 while True:
-    answer = GetAnswer()
-    if answer == "Посчитать":
-        a, op, b = GetOpAndNums()
+    print_last_result()
+    command = get_command()
+
+    if command == "Посчитать":
+        a, op, b = get_operation_and_operands()
 
         match op:
             case '+':
-                result = Addition(a, b)
+                result = addition(a, b)
             case '-':
-                result = Subtraction(a, b)
+                result = subtraction(a, b)
             case '*':
-                result = Multiplication(a, b)
+                result = multiplication(a, b)
             case '/':
-                result = Division(a, b)
+                result = division(a, b)
                 
-        print(result)
+        print_result(result)
+
         last_result, result = result, 0
-    elif answer == "История":
-        ShowHistory()
-    elif answer == "Выход":
+        
+        update_history(last_result)
+    elif command == "История":
+        show_history()
+    elif command == "Выход":
         farewell()
         break
+    elif command == "help":
+        show_commands()
