a84201046d93da7d129449893239633ae5609c1a
Auguste <longwonderstaff@gmail.com>
2026-04-24 19:48:23 +0300

    add advanced math ui

--- ui/input_handler.py
+++ ui/input_handler.py
@@ -1,6 +1,11 @@
 def get_operation_and_operands(last_result):
     print("""
-          Возможные операции: +, -, *, /
+          Возможные операции
+          ------------------
+          1) Базовые: +, -, *, /
+          2) Продвинутые: ^, sqrt, %, //
+          3) Операции над одним числом: abs, !, +-
+
 Введите Enter в качестве первого числа, чтобы продолжить работу над предыдущим числом
           """)
 
@@ -9,7 +14,11 @@ def get_operation_and_operands(last_result):
         a = last_result
     else:
         a = float(a)
+
     op = input("Введите операцию: ")
+    if op in ["abs", "!", "+-"]:
+        return a, op, 0
+    
     b = float(input("Введите второе число: "))
     
     return a, op, b
