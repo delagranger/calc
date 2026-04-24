6e4b63c81cc7bdcadc9a3c32a4a44c140df054aa
Auguste <longwonderstaff@gmail.com>
2026-04-24 15:36:54 +0300

    update/input

--- ui/input_handler.py
+++ ui/input_handler.py
@@ -1,12 +1,19 @@
-def GetOpAndNums():
-    print("Возможноные операции: +, -, *, /")
+def get_operation_and_operands(last_result):
+    print("""
+          Возможные операции: +, -, *, /
+Введите Enter в качестве первого числа, чтобы продолжить работу над предыдущим числом
+          """)
 
-    a = float(input("Введите первое число: "))
+    a = input("Введите первое число: ")
+    if a == "":
+        a = last_result
+    else:
+        a = float(a)
     op = input("Введите операцию: ")
     b = float(input("Введите второе число: "))
     
     return a, op, b
 
-def GetAnswer():
-    answer = input("Впишите действие (Посчитать, История, Выход): ")
+def get_command():
+    answer = input("Впишите команду (или help для просмотра команд): ")
     return answer
